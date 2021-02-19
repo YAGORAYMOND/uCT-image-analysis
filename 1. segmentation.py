8543f59baf21041b37eb1a44ffccb8390d0a94f0
YAGORAYMOND <51046145+YAGORAYMOND@users.noreply.github.com>
2021-02-19 09:49:25 +0100

    Update 1. segmentation.py

--- 1. segmentation.py
+++ 1. segmentation.py
@@ -1,4 +1,4 @@
-#import libraries
+#Import libraries
 import os
 import skimage as sk
 from skimage import io
@@ -7,24 +7,21 @@ from sklearn import mixture
 from skimage.morphology import disk
 from skimage.filters import median
 
-#*this process can be iterated*
+#*This process can be iterated for multiple files*
 
-#image import
-os.chdir("/path") #Directory path where stacks of u-CT reconstructions with VOI defined are located
-sample_name="sample.tif" #name of the sample to read (tif stack format)
-I_stack = sk.io.imread(sample_name) # read image using imread function
+#Image import
+os.chdir("/path") # Directory path where stacks of u-CT reconstructions with VOI defined are located
+sample_name="sample.tif" # Name of the sample to read (tif stack format)
+I_stack = sk.io.imread(sample_name) # Read image using imread function
 
-#segmentation
+#Segmentation
 I_stack_seg=[]
 for i in range(I_stack.shape[0]):
 
     I=I_stack[i]
 
-    # reshape training image from matrix to 1d array:
-    I_resh = I.reshape(I.shape[0]*I.shape[1],1).astype(np.uint8) #we are including the 3 dimensions MxNxL in a 1D array
-
-    #delete all the 0 values in the 1D array
-    Iresh1=I_resh[I_resh>0] #valors on iresh >0
+    # Reshape training image from matrix to 1d array:
+    I_resh = I.reshape(I.shape[0]*I.shape[1],1).astype(np.uint8) # We are including the 3 dimensions MxNxL in a 1D array
 
     #Fitting a gaussian mixture model (GMM) with 4 groups
     gmm4=mixture.GaussianMixture(n_components=4)
@@ -39,14 +36,14 @@ for i in range(I_stack.shape[0]):
     I_segmented1[I_segmented==2]=np.where(index==2)[0][0]
     I_segmented1[I_segmented==3]=np.where(index==3)[0][0]
 
-    #append the segmented slice (I_segmented1) to the global segmented matrix(I_stack_seg)
+    #Append the segmented slice (I_segmented1) to the global segmented matrix(I_stack_seg)
     I_stack_seg.append(I_segmented1)
 
 
-#image filtering
+#Image filtering
 I_stack_seg_filt=[]
 
 for i in range(I_stack.shape[0]):
     f=I_stack_seg[i].astype(np.uint8)
-    f=median(f,disk(5)) # 5pixel (=50um) disc median filter to eliminate the segmentation artefacts
+    f=median(f,disk(5)) # 5-pixel (=50um) disc median filter to eliminate the segmentation artefacts
     I_stack_seg_filt.append(f)
